71ab6d31bf57f9c4f3cc31918b19ad978eed29fa
PawlakAidan <pawlakah@dukes.jmu.edu>
2025-12-28 13:14:16 -0500

    Added WASD support and first-person mouse look

---
+++ player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+@export var mouse_sensitivity = 0.002
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+	   # Rotate the whole player left/right (Y axis)
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		# Rotate the camera up/down (X axis)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		# Clamp the vertical look so you don't flip upside down
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.5, 1.5)
+		
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
